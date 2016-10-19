a7a767cc858dc84679a7f8855c306f086c04d4e2
Harry van Haaren <harryhaaren@gmail.com>
2016-10-19 21:29:33 +0100

    light_set: function documentation improved

--- ctlr/ctlr.h
+++ ctlr/ctlr.h
@@ -72,7 +72,16 @@ uint32_t ctlr_dev_poll(struct ctlr_dev_t *dev);
  */
 int32_t ctlr_dev_disconnect(struct ctlr_dev_t *dev);
 
-/** Send Lights/LEDs feedback to device */
+/** Send Lights/LEDs feedback to device.
+ * The *light_id* is a value specific to the device that enumerates each
+ * available light. The *light_status* variable represents the state of
+ * the light, as a bitmask of 3 properties: blinking, brightness, colour.
+ * The top bit (1 << 31) indicates if the light should be blinking.
+ * The brightness (0x7F << 24) is a 0 to 127 brightness value.
+ * The remaining 16 bits are encoded as 0xRRGGBB in hex.
+ * Controllers should support these inputs as best they can for the given
+ * light_id.
+ */
 void ctlr_dev_light_set(struct ctlr_dev_t *dev,
 			uint32_t light_id,
 			uint32_t light_status);
