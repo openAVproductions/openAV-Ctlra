2e7ae7af4dc3271b13c81217f89d7063e1f89614
Harry van Haaren <harryhaaren@gmail.com>
2016-12-12 19:20:00 +0000

    vegas: refactor files and cleanup global

--- examples/vegas_mode/global.h
+++ examples/vegas_mode/global.h
@@ -2,6 +2,8 @@
 #ifndef VEGAS_MODE_GLOBAL_H
 #define VEGAS_MODE_GLOBAL_H
 
+#include "ctlr.h"
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -10,5 +12,14 @@ struct dummy_data {
 	float volume;
 	int playing[2];
 };
+
+/* Functions to poll / push state to the devs */
+void kontrol_x1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d);
+void kontrol_x1_func(struct ctlr_dev_t* dev, uint32_t num_events,
+		     struct ctlr_event_t** events, void *userdata);
+
+void kontrol_z1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d);
+void kontrol_z1_func(struct ctlr_dev_t* dev, uint32_t num_events,
+		     struct ctlr_event_t** events, void *userdata);
 
 #endif /* VEGAS_MODE_GLOBAL_H */
