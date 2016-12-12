70cec03515d6c3e2dffed37e54399bfc889903fb
Harry van Haaren <harryhaaren@gmail.com>
2016-12-12 18:44:57 +0000

    demo: supports 2 controllers with LEDs + input

--- demo.c
+++ demo.c
@@ -4,14 +4,49 @@
 #include <signal.h>
 
 #include "ctlr/ctlr.h"
+#include "ctlr/devices/ni_kontrol_z1.h"
+#include "ctlr/devices/ni_kontrol_x1_mk2.h"
+
+enum CHANNEL {
+	LEFT = 0,
+	RIGHT,
+};
+
+struct dummy_data {
+	uint64_t revision;
+	float volume;
+	int playing[2];
+};
+
+void kontrol_z1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d)
+{
+	uint32_t i;
+	int v = d->volume > 0.5f;
+	ctlr_dev_light_set(dev, 15, v * UINT32_MAX);
+	printf("light %d set to %d\n", 15, v);
+
+	ctlr_dev_light_flush(dev, 0);
+	return;
+
+	uint32_t iter = (int)((0.05) * 7.f);
+	for(i = 0; i < iter; i++) {
+		ctlr_dev_light_set(dev, 8 + i, UINT32_MAX);
+	}
+	for(; i < 7.0; i++) {
+		ctlr_dev_light_set(dev, 1 + i, 0);
+		ctlr_dev_light_set(dev, 8 + i, 0);
+	}
+}
 
 void kontrol_z1_func(struct ctlr_dev_t* dev,
 		     uint32_t num_events,
 		     struct ctlr_event_t** events,
 		     void *userdata)
 {
+	struct dummy_data *dummy = (void *)userdata;
 	(void)dev;
 	(void)userdata;
+	printf("%s\n", __func__);
 	for(uint32_t i = 0; i < num_events; i++) {
 		char *pressed = 0;
 		struct ctlr_event_t *e = events[i];
@@ -35,8 +70,9 @@ void kontrol_z1_func(struct ctlr_dev_t* dev,
 			printf("[%03d] slider %s (%d)\n",
 			       (int)(e->slider.value * 100.f),
 			       name, e->slider.id);
-			if(e->slider.id == 11) {
-				uint32_t iter = (int)((e->slider.value+0.05) * 7.f);
+			if(e->slider.id == NI_KONTROL_Z1_SLIDER_LEFT_FADER) {
+				dummy->volume = e->slider.value;
+				uint32_t iter = (int)((dummy->volume+0.05) * 7.f);
 				for(i = 0; i < iter; i++) {
 					ctlr_dev_light_set(dev, 1 + i, UINT32_MAX);
 					ctlr_dev_light_set(dev, 8 + i, UINT32_MAX);
@@ -64,8 +100,16 @@ void kontrol_z1_func(struct ctlr_dev_t* dev,
 			break;
 		};
 	}
+	dummy->revision++;
+}
 
+void kontrol_x1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d)
+{
+	uint32_t i;
+	int v = d->volume > 0.5f;
+	ctlr_dev_light_set(dev, 15, v * UINT32_MAX);
 	ctlr_dev_light_flush(dev, 0);
+	return;
 }
 
 void kontrol_x1_func(struct ctlr_dev_t* dev,
@@ -73,6 +117,7 @@ void kontrol_x1_func(struct ctlr_dev_t* dev,
 		     struct ctlr_event_t** events,
 		     void *userdata)
 {
+	struct dummy_data *dummy = (void *)userdata;
 	(void)dev;
 	(void)userdata;
 	for(uint32_t i = 0; i < num_events; i++) {
@@ -127,8 +172,7 @@ void kontrol_x1_func(struct ctlr_dev_t* dev,
 			break;
 		};
 	}
-
-	ctlr_dev_light_flush(dev, 0);
+	dummy->revision++;
 }
 
 
@@ -143,25 +187,36 @@ int main()
 {
 	signal(SIGINT, sighndlr);
 
+	struct dummy_data dummy;
+
 	struct ctlr_dev_t* dev_x1;
 	struct ctlr_dev_t* dev_z1;
 	//int dev_id = CTLR_DEV_SIMPLE;
 	//int dev_id = CTLR_DEV_NI_KONTROL_Z1;
 	//int dev_id = CTLR_DEV_NI_KONTROL_X1_MK2;
 	//int dev_id = CTLR_DEV_NI_MASCHINE_MIKRO_MK2;
-	void *userdata = 0x0;
 	void *future = 0x0;
-	dev_x1 = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_X1_MK2, kontrol_x1_func, userdata, future);
+	dev_x1 = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_X1_MK2,
+				  kontrol_x1_func,
+				  &dummy, future);
 	if(!dev_x1)
 		return -1;
-	dev_z1 = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_Z1, kontrol_z1_func, userdata, future);
+	dev_z1 = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_Z1,
+				  kontrol_z1_func,
+				  &dummy, future);
 	if(!dev_z1)
 		return -1;
 
 	printf("polling loop now..\n");
+	uint64_t controller_revision = 0;
 	while(!done) {
 		ctlr_dev_poll(dev_x1);
 		ctlr_dev_poll(dev_z1);
+		if(dummy.revision != controller_revision) {
+			kontrol_z1_update_state(dev_z1, &dummy);
+			kontrol_x1_update_state(dev_x1, &dummy);
+			controller_revision = dummy.revision;
+		}
 		usleep(100);
 	}
 
