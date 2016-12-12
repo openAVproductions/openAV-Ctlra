9312b96b10eb0904bd8e324b05f2f6910fe38367
Harry van Haaren <harryhaaren@gmail.com>
2016-12-12 22:36:19 +0000

    vegas: initialization of controllers from struct

--- examples/vegas_mode/global.h
+++ examples/vegas_mode/global.h
@@ -13,7 +13,11 @@ struct dummy_data {
 	int playing[2];
 };
 
+/* Typedefs so we can store the per-device functions in a struct, see
+ * vegas_mode.c ctlr_supported_t for details and how these are  used */
 typedef void (*update_state_cb)(struct ctlr_dev_t *dev, struct dummy_data *d);
+typedef void (*ctlr_poll_func)(struct ctlr_dev_t* dev, uint32_t num_events,
+			       struct ctlr_event_t** events, void *userdata);
 
 /* Functions to poll / push state to the devs */
 void kontrol_x1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d);
@@ -23,5 +27,9 @@ void kontrol_x1_func(struct ctlr_dev_t* dev, uint32_t num_events,
 void kontrol_z1_update_state(struct ctlr_dev_t *dev, struct dummy_data *d);
 void kontrol_z1_func(struct ctlr_dev_t* dev, uint32_t num_events,
 		     struct ctlr_event_t** events, void *userdata);
+
+void mm_update_state(struct ctlr_dev_t *dev, struct dummy_data *d);
+void mm_func(struct ctlr_dev_t* dev, uint32_t num_events,
+	     struct ctlr_event_t** events, void *userdata);
 
 #endif /* VEGAS_MODE_GLOBAL_H */
--- examples/vegas_mode/vegas_mode.c
+++ examples/vegas_mode/vegas_mode.c
@@ -22,46 +22,47 @@ struct ctlr_t {
 };
 TAILQ_HEAD(ctlr_list_t, ctlr_t);
 
+struct ctlr_supported_t {
+	enum ctlr_dev_id_t dev_id;
+	ctlr_poll_func ctlr_poll;
+	update_state_cb update_state;
+};
+
+static const struct ctlr_supported_t ctlr_supported[] = {
+	{CTLR_DEV_NI_KONTROL_Z1, kontrol_z1_func, kontrol_z1_update_state},
+	{CTLR_DEV_NI_KONTROL_X1_MK2, kontrol_x1_func, kontrol_x1_update_state},
+	//{CTLR_DEV_NI_MASCHINE_MIKRO_MK2, mm_func, mm_update_state},
+};
+#define CTLR_SUPPORTED_SIZE (sizeof(ctlr_supported) /\
+			     sizeof(ctlr_supported[0]))
+
 int main()
 {
 	signal(SIGINT, sighndlr);
 
 	struct dummy_data dummy;
+	void *future = 0x0;
 
 	struct ctlr_list_t ctlr_list;
 	TAILQ_INIT(&ctlr_list);
-	//int dev_id = CTLR_DEV_SIMPLE;
-	//int dev_id = CTLR_DEV_NI_KONTROL_Z1;
-	//int dev_id = CTLR_DEV_NI_KONTROL_X1_MK2;
-	//int dev_id = CTLR_DEV_NI_MASCHINE_MIKRO_MK2;
-	void *future = 0x0;
-	struct ctlr_dev_t* ctlr;
-	ctlr = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_X1_MK2,
-				kontrol_x1_func, &dummy, future);
-	if(ctlr) {
-		struct ctlr_t *dev = calloc(1, sizeof(struct ctlr_t));
-		if(!dev) return -1;
-		dev->ctlr = ctlr;
-		dev->update_state = kontrol_x1_update_state;
-		TAILQ_INSERT_TAIL(&ctlr_list, dev, list);
-	}
-	ctlr = ctlr_dev_connect(CTLR_DEV_NI_KONTROL_Z1, kontrol_z1_func,
-				&dummy, future);
-	if(ctlr) {
-		struct ctlr_t *dev = calloc(1, sizeof(struct ctlr_t));
-		if(!dev) return -1;
-		dev->ctlr = ctlr;
-		dev->update_state = kontrol_z1_update_state;
-		TAILQ_INSERT_TAIL(&ctlr_list, dev, list);
+
+	for(uint32_t i = 0; i < CTLR_SUPPORTED_SIZE; i++) {
+		const struct ctlr_supported_t* sup = &ctlr_supported[i];
+		printf("sup devid %d\n", sup->dev_id);
+		struct ctlr_dev_t* ctlr = ctlr_dev_connect(sup->dev_id,
+							   sup->ctlr_poll,
+							   &dummy,
+							   future);
+		if(ctlr) {
+			struct ctlr_t *dev = calloc(1, sizeof(struct ctlr_t));
+			if(!dev) return -1;
+			dev->ctlr = ctlr;
+			dev->update_state = sup->update_state;
+			TAILQ_INSERT_TAIL(&ctlr_list, dev, list);
+		}
 	}
 
-	int i = 0;
 	struct ctlr_t* dev;
-	TAILQ_FOREACH(dev, &ctlr_list, list) {
-		printf("%d, %p, %p\n", i, dev->ctlr, dev->update_state);
-		i++;
-	}
-
 	uint64_t controller_revision = 0;
 	while(!done) {
 		/* Poll all controllers */
