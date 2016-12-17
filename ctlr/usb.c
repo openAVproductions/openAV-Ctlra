d8efe1b169f34ffac67fdc62b2616a80c2d3a73a
Harry van Haaren <harryhaaren@gmail.com>
2016-12-17 13:43:46 +0000

    ctlr: usb improve warnings and remove prints

--- ctlr/usb.c
+++ ctlr/usb.c
@@ -37,7 +37,7 @@ int ctlr_dev_impl_usb_open(struct ctlr_dev_t *ctlr_dev, int vid, int pid,
 			printf("failed to get device descriptor");
 			goto fail;
 		}
-#if 1
+#if 0
 		printf("%04x:%04x (bus %d, device %d)",
 		       desc.idVendor, desc.idProduct,
 		       libusb_get_bus_number(dev),
@@ -69,7 +69,6 @@ int ctlr_dev_impl_usb_open(struct ctlr_dev_t *ctlr_dev, int vid, int pid,
 		printf("Error in claiming interface\n");
 		goto fail;
 	}
-	printf("got device OK\n");
 
 	/* enable auto management of kernel claiming / unclaiming */
 	if (libusb_has_capability(LIBUSB_CAP_SUPPORTS_DETACH_KERNEL_DRIVER)) {
@@ -79,7 +78,7 @@ int ctlr_dev_impl_usb_open(struct ctlr_dev_t *ctlr_dev, int vid, int pid,
 			return -1;
 		}
 	} else {
-		printf("Warning: auto kernel claim/unclaiming not supported\n");
+		printf("Ctlr: Warning: auto kernel claim/unclaiming not supported\n");
 	}
 
 	ret = libusb_claim_interface(handle, interface);
@@ -88,7 +87,7 @@ int ctlr_dev_impl_usb_open(struct ctlr_dev_t *ctlr_dev, int vid, int pid,
 		int kernel_active = libusb_kernel_driver_active(handle,
 							interface);
 		if(kernel_active)
-			printf("=> Kernel has claimed the interface."
+			printf("Ctlr => Kernel has already claimed the interface."
 			       "Stop other applications using this device and retry\n");
 		return -1;
 	}
