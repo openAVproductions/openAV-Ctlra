baa2441d54c0fe31897acf0b4f7dd2090a913db9
Harry van Haaren <harryhaaren@gmail.com>
2017-09-01 19:48:08 +0100

    usb: move usb info prints to CTLRA_INFO macro
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- ctlra/ctlra.c
+++ ctlra/ctlra.c
@@ -132,8 +132,8 @@ int32_t ctlra_dev_disconnect(struct ctlra_dev_t *dev)
 	struct ctlra_dev_t *dev_iter = ctlra->dev_list;
 
 	for(int i = 0; i < USB_XFER_COUNT; i++) {
-		printf("usb xfer count (type %d) = %d\n", i,
-		       dev->usb_xfer_counts[i]);
+		CTLRA_INFO(ctlra, "usb xfer count (type %d) = %d\n", i,
+			   dev->usb_xfer_counts[i]);
 	}
 
 	if(dev && dev->disconnect) {
