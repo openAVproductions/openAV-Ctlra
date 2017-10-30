77803bcb152d7bc9b9102aca13970a0f37e1e295
Harry van Haaren <harryhaaren@gmail.com>
2017-10-30 13:02:25 +0000

    midi: initial commit of generic MIDI backend
    This commit adds a new ctlra backend which is always accepted, and
    create generic MIDI I/O ports in ALSA midi. Any device which is
    connected to these generic MIDI ports will show up as Ctlra events
    to a Ctlra enabled program.
    
    Relates to #15.
    
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

---
+++ ctlra/devices/midi_generic.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2017, OpenAV Productions,
+ * Harry van Haaren <harryhaaren@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "impl.h"
+#include "midi.h"
+
+#define CONTROLS_SIZE 512
+#define LIGHTS_SIZE 512
+
+#define AKAI    0x09e8
+#define APC40   0x0073
+#define MPKMINI 0x007c
+
+#define CTLRA_DRIVER_VENDOR AKAI
+#define CTLRA_DRIVER_DEVICE MPKMINI
+
+
+/* Represents the the hardware device */
+struct midi_generic_t {
+	/* base handles usb i/o etc */
+	struct ctlra_dev_t base;
+	/* midi i/o */
+	struct ctlra_midi_t *midi;
+	/* current value of each controller is stored here */
+	float hw_values[CONTROLS_SIZE];
+	uint8_t lights[LIGHTS_SIZE];
+};
+
+static uint32_t midi_generic_poll(struct ctlra_dev_t *base)
+{
+	struct midi_generic_t *dev = (struct midi_generic_t *)base;
+	ctlra_midi_input_poll(dev->midi);
+	return 0;
+}
+
+int midi_generic_midi_input_cb(uint8_t nbytes, uint8_t * buf, void *ud)
+{
+	struct midi_generic_t *dev = (struct midi_generic_t *)ud;
+
+	switch(buf[0] & 0xf0) {
+	case 0x90: /* Note On */
+	case 0x80: /* Note Off */ {
+		struct ctlra_event_t event = {
+			.type = CTLRA_EVENT_BUTTON,
+			.button  = {
+				.id = buf[1],
+				.pressed = buf[0] >= 0x90,
+				//."velocity" = buf[2] / 127.f;
+			},
+		};
+		struct ctlra_event_t *e = {&event};
+		dev->base.event_func(&dev->base, 1, &e,
+				     dev->base.event_func_userdata);
+		} break;
+
+	case 0xb0: /* control change */ {
+		struct ctlra_event_t event = {
+			.type = CTLRA_EVENT_SLIDER,
+			.slider  = {
+				.id = buf[1],
+				.value = buf[2] / 127.f
+			},
+		};
+		struct ctlra_event_t *e = {&event};
+		dev->base.event_func(&dev->base, 1, &e,
+				     dev->base.event_func_userdata);
+		}
+		break;
+	};
+
+	/*
+	// TODO: figure out how to do feedback to the device
+	if(buf[1] == 0x37) {
+		uint8_t out[] = {0x90, 0x37, 1 + 2 * (buf[0] == 0x80)};
+		ctlra_midi_output_write(dev->midi, 3, out);
+	}
+	*/
+	return 0;
+}
+
+static const char *
+midi_generic_control_get_name(enum ctlra_event_type_t type,
+			      uint32_t control)
+{
+	return "generic";
+}
+
+static void midi_generic_light_set(struct ctlra_dev_t *base,
+				    uint32_t light_id,
+				    uint32_t light_status)
+{
+	struct midi_generic_t *dev = (struct midi_generic_t *)base;
+}
+
+void
+midi_generic_light_flush(struct ctlra_dev_t *base, uint32_t force)
+{
+	struct midi_generic_t *dev = (struct midi_generic_t *)base;
+}
+
+static int32_t
+midi_generic_disconnect(struct ctlra_dev_t *base)
+{
+	struct midi_generic_t *dev = (struct midi_generic_t *)base;
+	memset(dev->lights, 0, sizeof(dev->lights));
+	free(dev);
+	return 0;
+}
+
+struct ctlra_dev_info_t ctlra_midi_generic_info;
+
+struct ctlra_dev_t *
+ctlra_midi_generic_connect(ctlra_event_func event_func,void *userdata,
+			   void *future)
+{
+	(void)future;
+	struct midi_generic_t *dev = calloc(1, sizeof(struct midi_generic_t));
+	if(!dev)
+		goto fail;
+
+	dev->midi = ctlra_midi_open("Ctlra Midi Generic",
+				    midi_generic_midi_input_cb, dev);
+	if(dev->midi == 0) {
+		printf("Ctlra: error opening midi i/o\n");
+		goto fail;
+	}
+
+	dev->base.info = ctlra_midi_generic_info;
+
+	dev->base.poll = midi_generic_poll;
+	dev->base.disconnect = midi_generic_disconnect;
+	dev->base.light_set = midi_generic_light_set;
+	dev->base.light_flush = midi_generic_light_flush;
+
+	dev->base.event_func = event_func;
+	dev->base.event_func_userdata = userdata;
+
+	return (struct ctlra_dev_t *)dev;
+fail:
+	free(dev);
+	return 0;
+}
+
+
+struct ctlra_dev_info_t ctlra_midi_generic_info = {
+	.vendor    = "OpenAV",
+	.device    = "Midi Generic",
+	.vendor_id = CTLRA_DRIVER_VENDOR,
+	.device_id = CTLRA_DRIVER_DEVICE,
+	.size_x    = 512,
+	.size_y    = 512,
+	.get_name =  midi_generic_control_get_name,
+};
+
+CTLRA_DEVICE_REGISTER(midi_generic)
