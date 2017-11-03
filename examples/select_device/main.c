485c7e3a43cffa77184272ce45b2acbab2d34435
Harry van Haaren <harryhaaren@gmail.com>
2017-11-03 11:52:21 +0000

    select device: refactor to use app struct
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- examples/select_device/main.c
+++ examples/select_device/main.c
@@ -5,9 +5,18 @@
 
 #include <ctlra/ctlra.h>
 
+struct app_t
+{
+	GtkWidget *combo_vendor;
+	GtkWidget *combo_device;
+	GtkWidget *vbox;
+};
+
 static void
 on_changed (GtkComboBox *widget, gpointer user_data)
 {
+	struct app_t *app = user_data;
+
 	GtkComboBox *combo_vendor = widget;
 	if (gtk_combo_box_get_active (combo_vendor) == 0)
 		return;
@@ -19,53 +28,51 @@ on_changed (GtkComboBox *widget, gpointer user_data)
 	const char *devices[32];
 	int devs = ctlra_get_devices_by_vendor(vendor, devices, 32);
 
-	GtkWidget *combo_device = gtk_combo_box_text_new ();
-	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(combo_device),
+	app->combo_device = gtk_combo_box_text_new ();
+	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(app->combo_device),
 					"Choose a device");
 	for(int i = 0; i < devs; i++) {
-		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_device),
+		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(app->combo_device),
 						devices[i]);
 	}
-	gtk_combo_box_set_active (GTK_COMBO_BOX (combo_device), 0);
 
-	GtkWidget *vbox = user_data;
-	gtk_container_add (GTK_CONTAINER (vbox), combo_device);
-	gtk_widget_show_all(vbox);
+	gtk_container_add (GTK_CONTAINER (app->vbox), app->combo_device);
+	gtk_widget_show_all(app->vbox);
 
 	g_free (vendor);
 }
 
 static void
-activate (GtkApplication *app,
+activate (GtkApplication *gtk_app,
           gpointer        user_data)
 {
 	gint i;
 	GtkWidget *window;
-	GtkWidget *combo_vendor;
+	struct app_t *app = user_data;
 
-	window = gtk_application_window_new (app);
+	window = gtk_application_window_new (gtk_app);
 	gtk_window_set_title (GTK_WINDOW (window), "Ctlra Device Chooser");
 	gtk_window_set_default_size (GTK_WINDOW (window), 200, -1);
 	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
-	combo_vendor = gtk_combo_box_text_new ();
-	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(combo_vendor),
+	app->combo_vendor = gtk_combo_box_text_new ();
+	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(app->combo_vendor),
 					"Choose a Vendor");
 
 	const char *vendors[32];
 	int ret = ctlra_get_vendors(vendors, 32);
 	for (i = 0; i < ret; i++) {
-		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_vendor),
+		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(app->combo_vendor),
 						vendors[i]);
 	}
-	gtk_combo_box_set_active (GTK_COMBO_BOX (combo_vendor), 0);
+	gtk_combo_box_set_active (GTK_COMBO_BOX (app->combo_vendor), 0);
 
-	GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
-	g_signal_connect (combo_vendor, "changed", G_CALLBACK (on_changed),
-	                  vbox);
+	app->vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+	g_signal_connect (app->combo_vendor, "changed",
+			  G_CALLBACK(on_changed), app);
 
-	gtk_container_add (GTK_CONTAINER (window), vbox);
-	gtk_container_add (GTK_CONTAINER (vbox), combo_vendor);
+	gtk_container_add (GTK_CONTAINER (window), app->vbox);
+	gtk_container_add (GTK_CONTAINER (app->vbox), app->combo_vendor);
 	gtk_widget_show_all (window);
 }
 
@@ -73,14 +80,16 @@ activate (GtkApplication *app,
 int
 main (int argc, char **argv)
 {
-	GtkApplication *app;
+	GtkApplication *gtk_app;
 	int status;
 
-	app = gtk_application_new ("com.openavproductions.ctlra.example",
+	gtk_app = gtk_application_new ("com.openavproductions.ctlra.example",
 				   G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
+
+	struct app_t app = {0};
+	g_signal_connect (gtk_app, "activate", G_CALLBACK (activate), &app);
+	status = g_application_run (G_APPLICATION (gtk_app), argc, argv);
+	g_object_unref (gtk_app);
 
 	return status;
 }
