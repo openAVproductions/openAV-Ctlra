e0a8a45ae1f480bf3d6e720e0002c4d6db333419
Harry van Haaren <harryhaaren@gmail.com>
2018-11-03 06:06:45 +0000

    examples/mappa: update to more complex tests
    This adds 64 targets and 64 sources, and a more complex
    but messy testing method. Cleanup of this example app
    is needed.
    
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- examples/mappa/main.c
+++ examples/mappa/main.c
@@ -17,6 +17,28 @@ void sighndlr(int signal)
 	printf("\n");
 }
 
+static float varray[64];
+
+void sw_target_varray(uint32_t target_id, float value, void *token,
+		      uint32_t token_size, void *userdata)
+{
+	float *v = token;
+	*v = value;
+#if 0
+	printf("%p = %f\n", v, *v);
+	for(int i = 0; i < 10; i++)
+		printf("array %p: %d = %f\n", &varray[i], i, varray[i]);
+#endif
+}
+
+void sw_source_varray(float *value, void *token, uint32_t token_size,
+		      void *userdata)
+{
+	float *v = (float *)token;
+	//printf("%p = %f\n", token, *v);
+	*value = ((float)*v) > 0.5 ? 0xfffff : 0;
+}
+
 static float the_value;
 
 void sw_source_float_func_1(float *value, void *token, uint32_t token_size,
@@ -40,14 +62,16 @@ void sw_target_float_func(uint32_t target_id,
 			  uint32_t token_size,
 			  void *userdata)
 {
-#if 1
+#if 0
 	printf("%s: target id %u, value %f, token size %d\n",
 	       __func__, target_id, value, token_size);
 #endif
 	if(token_size == 8) {
-		uint64_t t = *(uint64_t *)token;
-		assert(t == 0xcafe);
+		uint64_t *t = (uint64_t *)token;
+		//assert(*t == 0xcafe);
 		assert(token_size == sizeof(uint64_t));
+		*t = !*t;
+		value = *t;
 	} else if (token_size == sizeof(token_test)) {
 		assert(memcmp(token, token_test, sizeof(token_test)) == 0);
 	}
@@ -75,6 +99,19 @@ register_targets(struct mappa_t *m, void *userdata)
 	uint64_t token = 0xcafe;
 	ret = mappa_target_add(m, &t, &tid, &token, sizeof(uint64_t));
 	assert(ret == 0);
+
+	/* targets 3 - 64 */
+#if 1
+	t.func = sw_target_varray;
+	for(int i = 3; i < 64; i++) {
+		char buf[32];
+		snprintf(buf, sizeof(buf), "t_%u", i);
+		t.name = buf;
+		void *token = &varray[i];
+		ret = mappa_target_add(m, &t, &tid, &token, sizeof(uint64_t));
+		assert(ret == 0);
+	}
+#endif
 
 	t.name = "t_3";
 	ret = mappa_target_add(m, &t, &tid, &token_test, sizeof(token_test));
@@ -109,6 +146,17 @@ register_feedback(struct mappa_t *m, void *userdata)
 	ret = mappa_source_add(m, &fb, &source_id, 0, 0);
 	if(ret != 0)
 		printf("MAP %d failed: sid %d\n", __LINE__, source_id);
+
+	for(int i = 4; i < 64; i++) {
+		float *token = &varray[i];
+		char buf[32];
+		snprintf(buf, sizeof(buf), "test_fb_%d", i);
+		fb.name = buf;
+		fb.func = sw_source_varray;
+		ret = mappa_source_add(m, &fb, &source_id, token, 8);
+		if(ret != 0)
+			printf("MAP %d failed: sid %d\n", __LINE__, source_id);
+	}
 
 	/* verify removal */
 	ret = mappa_source_remove(m, source_id);
